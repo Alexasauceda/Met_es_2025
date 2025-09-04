6dd192c6bee15dba7894202829134ccaf9d0cd33
alexa <alexa.morenos@uanl.edu.mx>
2025-09-04 07:56:45 -0600

    update

--- tarea.R
+++ tarea.R
@@ -5,9 +5,13 @@ data(iris)
 head(iris)
 summary(iris)
 data_sub <- subset(iris,Species %in% c("versicolor","virginica"))
+iris_especies <- data.frame(petal_length = data_sub$Petal.Length,
+                            species = data_sub$Species)
+
 table(data_sub$Species)
 head(data_sub)
 
+tapply(data_sub$Petal.Length,iris_especies)
 tapply(data_sub$Petal.Length,data_sub$Species,mean)
 tapply(data_sub$Petal.Length,data_sub$Species,sd)
 aggregate(Petal.Length ~ Species,data = data_sub,summary)
@@ -16,16 +20,15 @@ t.test(Petal.Length ~ Species,data = data_sub, var.equal=TRUE)
 t.test(Petal.Length ~ Species,data = data_sub, var.equal=FALSE)
 library(effsize)
 cohen.d(Petal.Length ~ Species,data=data_sub)
-
-df_vers <-subset(data_sub, Species =="versicolor")
-df_virg <-subset(data_sub, Species !="virginica")
+df_virg <-subset(iris_especies,species == "virginica")
+df_vers <-subset(iris_especies, species !="virginica")
 cohens_efecto <- function(x, y) {
   n1 <- length(x); n2 <- length(y)
   s1 <-sd(x); s2 <- sd(y)
   sp <- sqrt(((n1 - 1) *s1^2 + (n2 - 1)* s2^2)/(n1 + n2 - 2))
   (mean(x)- mean(y)) / sp
   }
-d_cal <- cohens_efecto(df_vers$Petal.Length, df_virg$Petal.Length)
+d_cal <- cohens_efecto(df_vers$petal_length, df_virg$petal_length)
 d_cal
 boxplot(Petal.Length ~ Species,data=data_sub,
         main="Comparacion de largo de petalo",
